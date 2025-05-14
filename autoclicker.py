e7b1cab4754feef57abef7616b7c59f508764451
Robin W Riimala <155627992+Wongpheng@users.noreply.github.com>
2025-05-14 10:52:18 +0200

    Add files via upload

---
+++ autoclicker.py
@@ -0,0 +1,25 @@
+import mouse, time, keyboard
+
+xValue = 160
+yValue = 300
+
+mouse.move(xValue, yValue)
+
+avbryt = False
+pause = False
+
+while avbryt == False:
+    time.sleep(0.05) # pause sånn at den ikke klikker for fort
+
+    if pause == False:
+        mouse.click()
+
+    if keyboard.is_pressed("p"):
+        pause = True
+
+    if keyboard.is_pressed("s"):
+        pause = False
+
+    if keyboard.is_pressed("q"):
+        avbryt = True
+    
